74429ef97c7b2aaf404b81add02d2a203df4217b
Vios <i.svoboda.vios@gmail.com>
2022-09-06 01:29:19 +0300

    Try

--- Zhemanov_Alexandr_dz_8/app.js
+++ Zhemanov_Alexandr_dz_8/app.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 
 let fitlerPopup = document.querySelector('.filterPopup');
 let fitlerLabel = document.querySelector('.filterLabel');
@@ -28,3 +28,88 @@ let filterSizeWrap = document.querySelector('.filterSizeWrap');
 filterSizeWrap.addEventListener('click', function() {
     filterSizes.classList.toggle('hidden');
 });
+
+
+const basket = getBasketApi();
+const featuredItems = document.querySelector(".featuredItems");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// add jdoc
+function getBasketApi() {
+  let productMap = new Map();
+  let productAddEvent = () => {};
+  
+  /**
+   * @return {Array}
+   */
+  function getProducts() {
+    return Array.from(productMap);
+  }
+
+  function getResultOneTypeProduct(name) {
+    if (!productMap.has(name)) {
+      return NaN;
+    }
+
+    let [count, price] = productMap.get(name);
+    return price * count;
+  }
+
+  function getResultAllProduct() {
+    let all = 0;
+    
+    Array.from(productMap.keys()).forEach((key) => {
+      all += getResultOneTypeProduct(key)
+    });
+
+    return all;
+  }
+
+  function getProductCount() {
+    let all = 0;
+    
+    productMap.forEach(([count, ]) => {
+      all += count;
+    })
+    return all;
+  }
+
+  function addProduct(name, price) {
+    if (productMap.has(name)) {
+      const [count, cPrice] = productMap.get(name);
+      productMap.set(name, [count + 1, cPrice]);
+    } else {
+      productMap.set(name, [1, price])
+    }
+
+    productAddEvent(getProductCount()); 
+  }
+  
+  function onProductAddEvent(fn) {
+    productAddEvent = fn;
+  }
+  return {
+    getProducts,
+    getProductCount,
+    getResultOneTypeProduct,
+    getResultAllProduct,
+    addProduct,
+    onProductAddEvent
+  }; 
+}
